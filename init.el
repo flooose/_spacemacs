09f113536bbcb70ee729205675c766396213ae5f
christopher floess <skeptikos@gmail.com>
2016-07-01 11:52:44 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,56 @@
+;; what time is it?
+(display-time-mode 1)
+
+;; we don't always want to do this manually
+(toggle-frame-maximized)
+
+;; cursor to bar
+(setq-default cursor-type 'bar)
+
+;; get rid of temporary files
+(setq temporary-file-directory "~/tmp")
+(setq create-lockfiles nil)
+(setq backup-directory-alist
+			`((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+			`((".*" ,temporary-file-directory t)))
+
+;; numbers and stuff
+(global-linum-mode t)
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;; predictable behaviour for guests
+(delete-selection-mode 1)
+
+;; KEYBINDINGS
+
+;; make working with strings easier
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;; taken from http://emacsredux.com/blog/2013/05/30/joining-lines/
+(defun top-join-line ()
+  "Join the current line with the line beneath it."
+  (interactive)
+  (delete-indentation 1))
+(global-set-key (kbd "C-^") 'top-join-line)
+
+;; rgrep
+(global-set-key (kbd "C-, g") 'rgrep)
+
+(defun flooose-open-line ()
+  "Open line below current line and go to it"
+  (interactive)
+  (end-of-line)
+  (newline-and-indent))
+(global-set-key (kbd "C-<return>") 'flooose-open-line)
+(global-set-key (kbd "C-o") 'flooose-open-line)
+
+(defun flooose-open-line-above ()
+  "Open line above and got to it"
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (previous-line))
+(global-set-key (kbd "C-S-<return>") 'flooose-open-line-above)
+(global-set-key (kbd "C-S-o") 'flooose-open-line-above)
