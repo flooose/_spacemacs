885eff02cdb6eec68998e58ae9ef7fac57072cbd
christopher floess <chris.floess@jjff.de>
2017-08-04 10:29:55 +0200

    Add multi-cursor support

--- init.el
+++ init.el
@@ -243,3 +243,8 @@
                                      (omnisharp-start-omnisharp-server default-directory))
                                  (flycheck-mode)
                                  ))
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-:") 'mc/mark-all-like-this)
